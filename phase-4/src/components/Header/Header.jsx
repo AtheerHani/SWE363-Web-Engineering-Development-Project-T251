024c7ff2a87b21d5503889025fe7502d27d9568f
Dana-Alyahya <s202279460@kfupm.edu.sa>
2025-11-16 14:41:16 +0300

    Login and sign up edit

--- phase-4/src/components/Header/Header.jsx
+++ phase-4/src/components/Header/Header.jsx
@@ -3,31 +3,31 @@ import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleRentYourSpace = () => {
-        // Navigate to signup or listing creation page
-        navigate("/signup");
-    };
+  const handleRentYourSpace = () => {
+    // Navigate to signup or listing creation page
+    navigate("/signup");
+  };
 
-    return (
-        <header className="header">
-            <div className="header-left">
-                <img src="/assets/icons/logo.png" alt="Hujra Logo" className="logo" />
-            </div>
+  return (
+    <header className="header">
+      <div className="header-left">
+        <img src="/assets/icons/logo.png" alt="Hujra Logo" className="logo" />
+      </div>
 
-            <nav className="nav-links">
-                <a href="#">Find a space</a>
-                <a href="#">Learn More</a>
-            </nav>
+      <nav className="nav-links">
+        <a href="#">Find a space</a>
+        <a href="#">Learn More</a>
+      </nav>
 
-            <div className="header-right">
-                <button className="rent-button" onClick={handleRentYourSpace}>
-                    Rent Your Space
-                </button>
-            </div>
-        </header>
-    );
+      <div className="header-right">
+        <button className="rent-button" onClick={handleRentYourSpace}>
+          Rent Your Space
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
